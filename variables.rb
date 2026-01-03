fa05f4ecf6ae09a6838690768106dbf056fcca1c
Jayavel <jayavel1520@gmail.com>
2026-01-03 09:55:49 +0530

    Add variables.rb file with personal introduction and programming details

---
+++ variables.rb
@@ -0,0 +1,10 @@
+name = "Jayavel"
+age = 21
+programming_language = "Ruby"
+
+puts "My name is #{name} and I am #{age} years old."
+puts "I am learning #{programming_language} programming."
+puts "I wish to be a successful developer one day."
+puts "My dream is to create impactful software that helps people."
+puts "Thank you for reading my introduction!"
+puts "Have a great day! " + name
