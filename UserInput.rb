225b2de281e1c8174a2ff3a93017b174b2ff628e
Jayavel <jayavel1520@gmail.com>
2026-01-03 12:04:44 +0530

    Add prompts for favorite color and CGPA in UserInput.rb

--- UserInput.rb
+++ UserInput.rb
@@ -3,3 +3,11 @@ name = gets.chomp()
 print "Enter your age: "
 age = gets.chomp().to_i
 puts "Hello, #{name}! You are #{age} years old."
+
+print "Enter your favorite color: "
+color = gets.chomp()
+puts "Your favorite color is #{color}."
+
+print "Enter your CGPA: "
+cgpa = gets.chomp().to_f
+puts "Your CGPA is #{cgpa}."
