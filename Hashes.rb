0e986493afc3000373a39da3c3303c06cbdfb954
Jayavel <jayavel1520@gmail.com>
2026-01-03 15:50:21 +0530

    Add Hashes.rb file to manage student data with key-value pairs

---
+++ Hashes.rb
@@ -0,0 +1,30 @@
+student =  {
+  "name" => "Jayavel",
+  "age" => 21,
+  "course" => "B.E Computer Science",
+  "sound" => {
+    "greeting" => "Hello, welcome to the world of Ruby!",
+    "farewell" => "Goodbye! See you next time."
+  }
+}
+
+puts student["name"]
+puts student["age"]
+puts student["course"]
+puts student["sound"]["greeting"]
+puts student["sound"]["farewell"]
+
+student["jazz"] = "Saxaphone" # adding new key-value pair
+student["age"] = 22 # updating existing key-value pair
+puts student.inspect
+
+student.delete("course")
+puts student.inspect
+student.each do |key, value|
+  puts "#{key}: #{value}"
+end
+
+puts student.keys.inspect
+puts student.values.inspect
+
+puts student.value?("Jayavel")
