85e8a193200f498889ab0ef4a7091db5f7f2c4a3
Jayavel <jayavel1520@gmail.com>
2026-01-03 17:13:33 +0530

    Add Loops.rb file with examples of while, until, infinite, and for loops

---
+++ Loops.rb
@@ -0,0 +1,37 @@
+# # # # while loops
+# # # i = 1
+# # # while i < 6 do
+# # #   puts i
+# # #   i += 1
+# # # end
+
+# # # until loops
+# # # j = 1
+# # # until j > 5 do
+# # #   puts j
+# # #   j += 1
+# # # end
+
+# # # infinite loops with break
+# # i = 1
+# # loop do
+# #   puts i
+# #   i += 1
+# #   if i > 5
+# #     break
+# #   end
+# # end
+
+# # for loops
+# # for k in 1...5 do
+# #   puts k
+# # end
+
+
+# age = 150
+# unless age >= 18
+#   puts "You are a minor."
+# else
+#   puts "You are an adult."
+# end
+
