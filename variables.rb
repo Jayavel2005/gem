c389b92fc0919500278f59cad6dc74e75cd52d2b
Jayavel <jayavel1520@gmail.com>
2026-01-03 09:56:29 +0530

    Update name variable in variables.rb to personalize introduction

--- variables.rb
+++ variables.rb
@@ -6,5 +6,6 @@ puts "My name is #{name} and I am #{age} years old."
 puts "I am learning #{programming_language} programming."
 puts "I wish to be a successful developer one day."
 puts "My dream is to create impactful software that helps people."
+name = "Mike"
 puts "Thank you for reading my introduction!"
 puts "Have a great day! " + name
