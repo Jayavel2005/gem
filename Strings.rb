7f5827ac0267f95f77910f1df48b7202b56094e1
Jayavel <jayavel1520@gmail.com>
2026-01-03 10:32:06 +0530

    Add Strings.rb file with string manipulation methods and examples

---
+++ Strings.rb
@@ -0,0 +1,17 @@
+name = "Hello I am \"Jayavel\"."
+puts name.upcase()
+puts name.downcase()
+puts name.strip()
+puts name.length()
+puts name.reverse()
+puts name.include? "J"
+puts name.count('a')
+puts name[0..name.length]
+
+puts name.replace("jayavel")
+
+
+name.insert(0, "Welcome! ")
+puts name.upcase()
+puts name[0..3]
+puts name.index("Welc")
