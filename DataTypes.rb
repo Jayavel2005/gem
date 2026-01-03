99497602b2d8c36e95bfc89a2df1e9c264a8e0d6
Jayavel <jayavel1520@gmail.com>
2026-01-03 10:12:12 +0530

    Add DataTypes.rb file with personal details and attributes

---
+++ DataTypes.rb
@@ -0,0 +1,14 @@
+name = "Jayavel"
+age = 21
+gpa = 9.07
+isStudent = true
+isTall = false
+isMuscular = true
+flaws = nil
+
+puts "Name: #{name}"
+puts "Age: #{age}"
+puts "GPA: #{gpa}"
+puts "Is Student: #{isStudent}"
+puts "Is Tall: #{isTall}"
+puts "Is Muscular: #{isMuscular}"
