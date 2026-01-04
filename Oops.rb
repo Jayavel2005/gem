98016d1660866148717f3795ab9251d20b665b3c
Jayavel <jayavel1520@gmail.com>
2026-01-04 18:40:33 +0530

    Refactor Oops.rb to comment out Person class example and improve Mobile class readability; add MathUtils class methods

--- Oops.rb
+++ Oops.rb
@@ -1,41 +1,56 @@
-# class Person
-#   attr_accessor :name, :age
-#   def initialize(name, age)
-#     @name = name
-#     @age = age
-#   end
+# # class Person
+# #   attr_accessor :name, :age
+# #   def initialize(name, age)
+# #     @name = name
+# #     @age = age
+# #   end
   
-#   def display_info
-#     puts "Name: #{@name}, Age: #{@age}"
+# #   def display_info
+# #     puts "Name: #{@name}, Age: #{@age}"
+# #   end
+# # end
+
+# # person1 = Person.new("Alice", 30)
+
+# # puts person1.name
+# # person1.age = 31
+# # person1.display_info
+
+# class Mobile
+#   attr_reader :brand, :price
+#   def initialize(brand, price)
+#     @brand = brand
+#     @price = price
+#   end
+
+#   def discount(percent)
+#      @price -= @price * percent / 100
+#   end
+
+#   def details
+#     puts "Brand: #{@brand}, Price: #{@price}"
 #   end
 # end
 
-# person1 = Person.new("Alice", 30)
+# mobile1 = Mobile.new("Samsung", 500)
+# mobile1.details
+# puts mobile1.discount(10)
+# puts mobile1.brand
 
-# puts person1.name
-# person1.age = 31
-# person1.display_info
 
-class Mobile
-  attr_reader :brand, :price
-  def initialize(brand, price)
-    @brand = brand
-    @price = price
+# puts mobile1
+
+class MathUtils
+  def self.sqaure(num)
+    return num * num 
   end
 
-  def discount(percent)
-     @price -= @price * percent / 100
-  end
 
-  def details
-    puts "Brand: #{@brand}, Price: #{@price}"
+  def cube(num)
+    return num * num * num 
   end
 end
 
-mobile1 = Mobile.new("Samsung", 500)
-mobile1.details
-puts mobile1.discount(10)
-puts mobile1.brand
-
-
-puts mobile1
+puts MathUtils.sqaure(4)
+math_util = MathUtils.new()
+puts math_util.cube(3)
